d387e651d6d9780103d7a5defaf94a0305efae62
LamanHashimova <112489993+LamanHashimova@users.noreply.github.com>
2022-11-22 20:31:01 +0400

    Add files via upload

---
+++ CodeJsTask/app.js
@@ -0,0 +1,52 @@
+// Task 1
+// let confirmation= confirm("Press a Ok button if you want to see website!")
+// if (confirmation== true) {
+//  document.body.style.display='block'
+// } else {
+//     document.body.style.display='none' 
+// }
+
+//   Task2 
+   
+//    let personage = prompt("Enter your age...");
+//     if (personage === null || personage ==="") {
+//      alert('Please enter your age')
+//     }
+//     else if(personage<18){
+//         alert('You can not enter the website')
+//     }else{
+//         document.getElementById('Welcome').innerHTML='Welcome our Website'
+//     }
+
+
+// Task 3
+// function Check(){
+//     let num= document.getElementById('Academy').value;
+//     if(num===""|| num===null){
+//         alert('Xais edirik qurup nomrenizi daxil edin');
+//     }
+//     else if(num.length< 4){
+//         alert(' Qurup nomrenizi duzgun daxil edin')
+//     }
+//     let numpartofvalue= num.slice(-3);
+//     if(isNaN(Number(numpartofvalue))){
+//         alert('Daxil etdiyiniz qurup nomresi duzgun deil')
+//     }else
+//     {
+//         document.getElementById('Result').innerHTML=`Welcome ${num}`;
+//         if(numpartofvalue>=100 && numpartofvalue <200){
+//             document.getElementById('Result').style.color='blue';
+           
+//         }
+//         else if(numpartofvalue>=200 && numpartofvalue<300){
+//             document.getElementById('Result').style.color='red';
+//         }
+//         else if(numpartofvalue>=300 && numpartofvalue<400){
+//             document.getElementById('Result').style.color='green';
+//         }else{
+//             document.getElementById('Result').style.color='black';
+//             document.getElementById('Result').innerHTML=`${num} Qurupu movcud deil`;
+//         }
+//     }
+   
+// }
